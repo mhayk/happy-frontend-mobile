403c748248011527107ce6ceb77a78a233816f7c
Mhayk Whandson <hi@mhayk.com>
2020-10-20 15:33:08 +0100

    The font Nunito has been configured and used.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+import { useFonts } from 'expo-font';
 
 import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  })
+
+  if(!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <MapView
@@ -70,7 +81,8 @@ const styles = StyleSheet.create({
 
   calloutText: {
     color: '#0089a5',
-    fontSize: 14
+    fontSize: 14,
+    fontFamily: 'Nunito_700Bold'
   },
 
   footer: {
@@ -93,6 +105,7 @@ const styles = StyleSheet.create({
   },
 
   footerText: {
+    fontFamily: 'Nunito_700Bold',
     color: '#8fa7b3'
   },
 
