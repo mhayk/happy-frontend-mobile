8797c030b8b666c7d4618690b6cb283b1eb69b69
Mhayk Whandson <hi@mhayk.com>
2020-10-22 10:36:13 +0100

    The conditional to evaluate if open on weekends has been created and styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/OrphanageDetails.tsx
+++ src/pages/OrphanageDetails.tsx
@@ -105,10 +105,19 @@ export default function OrphanageDetails() {
             <Feather name="clock" size={40} color="#2AB5D1" />
             <Text style={[styles.scheduleText, styles.scheduleTextBlue]}>Segunda à Sexta {orphanage.opening_hours}</Text>
           </View>
-          <View style={[styles.scheduleItem, styles.scheduleItemGreen]}>
+
+          {orphanage.open_on_weekends ? (
+            <View style={[styles.scheduleItem, styles.scheduleItemGreen]}>
             <Feather name="info" size={40} color="#39CC83" />
             <Text style={[styles.scheduleText, styles.scheduleTextGreen]}>Atendemos fim de semana</Text>
           </View>
+          ) : (
+            <View style={[styles.scheduleItem, styles.scheduleItemRed]}>
+            <Feather name="info" size={40} color="#FF669D" />
+            <Text style={[styles.scheduleText, styles.scheduleTextRed]}>Não atendemos fim de semana</Text>
+          </View>
+          )}
+          
         </View>
 
         {/* <RectButton style={styles.contactButton} onPress={() => {}}>
@@ -209,6 +218,13 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
 
+  scheduleItemRed: {
+    backgroundColor: '#FEF6F9',
+    borderWidth: 1,
+    borderColor: '#FFBCD4',
+    borderRadius: 20,
+  },
+
   scheduleText: {
     fontFamily: 'Nunito_600SemiBold',
     fontSize: 16,
@@ -222,6 +238,10 @@ const styles = StyleSheet.create({
 
   scheduleTextGreen: {
     color: '#37C77F'
+  },
+
+  scheduleTextRed: {
+    color: '#FF669D'
   },
 
   contactButton: {
