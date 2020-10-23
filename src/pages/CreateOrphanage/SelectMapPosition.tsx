e27c75a9e9a39bf84d4bcec39f31698238ae6667
Mhayk Whandson <hi@mhayk.com>
2020-10-23 22:45:05 +0100

    The longitude and latitude has been updated.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/CreateOrphanage/SelectMapPosition.tsx
+++ src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -23,8 +23,8 @@ export default function SelectMapPosition() {
     <View style={styles.container}>
       <MapView 
         initialRegion={{
-          latitude: -27.2092052,
-          longitude: -49.6401092,
+          latitude: 51.4665874,
+          longitude: -0.3473856,
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
         }}
